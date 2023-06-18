83910feebba1fa151493ead45094e85b1abeb9d3
GyroZepelix <gamingskullgamer@gmail.com>
2023-06-18 17:07:05 +0200

    Initial Commit

---
+++ src/main/java/com/gyro/gyroswogartifacts/GyrosWogArtifacts.java
@@ -0,0 +1,43 @@
+package com.gyro.gyroswogartifacts;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+import static com.gyro.gyroswogartifacts.GyrosWogArtifacts.MODID;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(MODID)
+public class GyrosWogArtifacts {
+    // Directly reference a slf4j logger
+    public static final String MODID = "gyroswogartifacts";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public GyrosWogArtifacts() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+}
